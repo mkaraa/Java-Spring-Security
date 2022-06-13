747a678c8f5a8167f31e8ce796b59e644ccb241f
mkaraa <metehan.kara95@gmail.com>
2022-06-14 00:13:02 +0300

    SimpleGrantedAuthorites implemented

--- src/main/java/com/sp/security/Java/Spring/Security/config/ApplicationSecurityConfigurer.java
+++ src/main/java/com/sp/security/Java/Spring/Security/config/ApplicationSecurityConfigurer.java
@@ -8,12 +8,14 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 
 @Configuration
@@ -28,15 +30,23 @@ public class ApplicationSecurityConfigurer extends WebSecurityConfigurerAdapter 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()   //TODO: next
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*")
                 .permitAll()
-                .antMatchers("/api/**").hasRole(ApplicationUserRoles.STUDENT.name())
-                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ApplicationUserRoles.ADMIN.name(),ApplicationUserRoles.ADMINTRAINEE.name())
+                .antMatchers("/api/**")
+                .hasRole(ApplicationUserRoles.STUDENT.name())
+                .antMatchers(HttpMethod.DELETE, "/management/api/**")
+//                .hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+                .hasAnyRole(ApplicationUserRoles.ADMIN.name())
+                .antMatchers(HttpMethod.POST, "/management/api/**")
+//                .hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+                .hasAnyRole(ApplicationUserRoles.ADMIN.name())
+                .antMatchers(HttpMethod.PUT, "/management/api/**")
+                .hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.GET, "/management/api/**")
+                .hasAnyRole(ApplicationUserRoles.ADMIN.name(), ApplicationUserRoles.ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -45,39 +55,25 @@ public class ApplicationSecurityConfigurer extends WebSecurityConfigurerAdapter 
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("linda")
-                .password(passwordEncoder().encode("password"))
+        auth.inMemoryAuthentication().withUser("linda").password(passwordEncoder().encode("password"))
                 .roles(ApplicationUserRoles.ADMIN.name());
+//                .authorities(ApplicationUserRoles.ADMIN.getSimpleGrantedAuthorities().toString()
 
-        auth.inMemoryAuthentication()
-                .withUser("smith")
-                .password(passwordEncoder().encode("password"))
+        auth.inMemoryAuthentication().withUser("smith").password(passwordEncoder().encode("password"))
                 .roles(ApplicationUserRoles.STUDENT.name());
+//                .authorities(ApplicationUserRoles.STUDENT.getPermissions().toString());
 
-        auth.inMemoryAuthentication()
-                .withUser("tom")
-                .password(passwordEncoder().encode("password"))
+        auth.inMemoryAuthentication().withUser("tom").password(passwordEncoder().encode("password"))
                 .roles(ApplicationUserRoles.ADMINTRAINEE.name());
+//                .authorities(ApplicationUserRoles.ADMINTRAINEE.getPermissions().toString());
     }
 
     @Override
     protected UserDetailsService userDetailsService() {
-        UserDetails userMete = User.builder()
-                .username("smith")
-                .password(passwordEncoder().encode("password"))
-                .roles(ApplicationUserRoles.STUDENT.name())
-                .build();
+        UserDetails userMete = User.builder().username("smith").password(passwordEncoder().encode("password")).roles(ApplicationUserRoles.STUDENT.name()).build();
 
-        UserDetails userLinda = User.builder()
-                .username("linda")
-                .password(passwordEncoder().encode("password"))
-                .roles(ApplicationUserRoles.ADMIN.name())
-                .build();
+        UserDetails userLinda = User.builder().username("linda").password(passwordEncoder().encode("password")).roles(ApplicationUserRoles.ADMIN.name()).build();
 
-        return new InMemoryUserDetailsManager(
-                userMete,
-                userLinda
-        );
+        return new InMemoryUserDetailsManager(userMete, userLinda);
     }
 }
